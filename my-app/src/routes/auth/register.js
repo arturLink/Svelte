3aaa39ebf1fc8ad4aeebd37ad5666f04c47372ff
vafle <vafle@ARTURIK>
2023-12-11 01:02:00 +0200

    Done

--- my-app/src/routes/auth/register.js
+++ my-app/src/routes/auth/register.js
@@ -6,7 +6,7 @@ export function post(req, res) {
     api.post('users', user ).then(response => {
         if (response.user) {
             req.session.user = response.user;
-        }
+        }//
 
         res.setHeader('Content-Type', 'application/json');
 
